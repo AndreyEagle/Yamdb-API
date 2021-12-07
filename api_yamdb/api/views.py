d4276bef60a24ce74c127248a0248be750292c3f
Komarova Tatyana <LambdaKomar@yandex.ru>
2021-12-07 09:35:07 +0300

    Добавила модели, представления, поинты

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from reviews.models import Category, Genre, Title
 
-# Create your views here.
+from .serializers import (CategorySerializer,
+                          GenreSerializer,TitleCreateSerializer,
+                          TitleSerializer)
+from rest_framework import filters, status, viewsets
+from rest_framework.response import Response
+from reviews.models import Category, Genre, Title
+from .serializers import (CategorySerializer,
+                          GenreSerializer, TitleCreateSerializer,
+                          TitleSerializer)
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name',)
+
+    def get_category(self, request, slug):
+        category = self.get_object()
+        serializer = CategorySerializer(category)
+        category.delete()
+        return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name',)
+
+    def get_genre(self, request, slug):
+        category = self.get_object()
+        serializer = CategorySerializer(category)
+        category.delete()
+        return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
+
+class TitleViewSet(viewsets.ModelViewSet):
+    queryset = Title.objects.all()
+    serializer_class = TitleSerializer
+
+    def get_serializer_class(self):
+        if self.request.method in ('POST', 'PATCH',):
+            return TitleCreateSerializer
+        return TitleSerializer
