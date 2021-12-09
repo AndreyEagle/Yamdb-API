dcf9891e7a49861311ec186a2cfdcb3e56720b0e
Andrey Orlov <orlov.lionel@yandex.ru>
2021-12-09 14:05:10 +0400

    Fix requirements

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -23,7 +23,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework_simplejwt',
     'reviews.apps.ReviewsConfig',
     'api.apps.ApiConfig',
     'django_filters'
