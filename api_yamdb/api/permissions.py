70ca40bd0c30681d10ff3d402a77c828b4405656
Рустем <rstmgrv@gmail.com>
2021-12-10 12:21:25 +0300

    коммит перед правками

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -5,7 +5,7 @@ class IsAdmin(BasePermission):
     def has_permission(self, request, view):
         return bool(
             request.user.is_authenticated
-            and request.user.role == 'admin'
+            and request.user.is_admin
             or request.user.is_superuser
         )
 
@@ -20,4 +20,13 @@ class AuthorOrReadOnly(BasePermission):
     def has_object_permission(self, request, view, obj):
         return (
             request.method in SAFE_METHODS
-            or (obj.author == request.user or request.user.role == 'moderator' or request.user.role == 'admin'))
+
+            or (obj.author == request.user
+                or request.user.is_moderator
+                or request.user.is_admin)
+        )
+
+class ReadOnly(BasePermission):
+
+    def has_permission(self, request, view):
+        return request.method in SAFE_METHODS
